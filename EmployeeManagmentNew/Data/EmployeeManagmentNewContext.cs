dcb3519294042c15098534482c4923533651e95f
Priyanshi Kothari <priyanshi@promactinfo.com>
2019-03-08 10:28:59 +0530

    add my all files

---
+++ EmployeeManagmentNew/Data/EmployeeManagmentNewContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EmployeeManagmentNew.Models;
+
+namespace EmployeeManagmentNew.Models
+{
+    public class EmployeeManagmentNewContext : DbContext
+    {
+        public EmployeeManagmentNewContext (DbContextOptions<EmployeeManagmentNewContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EmployeeManagmentNew.Models.Department> Department { get; set; }
+
+        public DbSet<EmployeeManagmentNew.Models.Employee> Employee { get; set; }
+    }
+}
---
+++ EmployeeManagmentNew/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace EmployeeManagmentNew.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        [Required] public string LastName { get; set; }
+        [Required] public string FirstName { get; set; }
+        [Required] public string Address { get; set; }
+        [Required] public string Qualification { get; set; }
+        [Display(Name = "ContectNumber")]
+        [Required(ErrorMessage = "Phone Number Required!")]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$",
+                    ErrorMessage = "Entered phone format is not valid.")]
+        public String ContectNumber { get; set; }
+        public int DepartmentId { get; set; }
+        public virtual Department Department { get; set; }
+    }
+}
